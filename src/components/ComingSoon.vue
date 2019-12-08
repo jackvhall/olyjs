e2ab7d6d2275d8ac4acbefc577e86ffabe93ef25
Vincent Hall <jackvhall@outlook.com>
2019-12-07 21:12:12 -0800

    coming soon component added

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="container mx-auto border-blue-100">
+  <div class="container mx-auto">
     <div>
-      <span class="text-5xl font-thin text-gray-800">Oly</span>
+      <span class="text-5xl font-hairline text-gray-800">Oly</span>
       <span class="text-5xl font-bold text-gray-800">JS</span>
     </div>
     <div>{{ msg }}</div>
     <div class="mt-12">
       <p>While we work on this site, consider checking us out at the following:</p>
-      <div class="flex justify-between align-middle">
-        <i class="fab fa-meetup"></i>
+      <div class="flex justify-center align-middle mt-10">
+        <a href="https://www.meetup.com/Olympia-Front-end-Development-Meetup-Group/">
+          <i class="fab fa-meetup inline-block text-5xl text-red-600 hover:text-red-400"></i>
+        </a>
+        <a href="https://github.com/OlyJS">
+          <i class="fab fa-github inline-block text-5xl text-gray-800 hover:text-gray-600 ml-4"></i>
+        </a>
       </div>
     </div>
   </div>
@@ -25,4 +30,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.fab {
+  transition: 3s all;
+}
+.fab:hover {
+  transform: translateY(-4px);
+}
 </style>
