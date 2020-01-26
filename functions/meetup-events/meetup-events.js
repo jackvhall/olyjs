a03eb2594218a43ab3fd7861d8b2cdf71ea664c3
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-01-26 10:26:23 -0800

    Change to server-side unauthenticated API request

--- functions/meetup-events/meetup-events.js
+++ functions/meetup-events/meetup-events.js
@@ -1,25 +1,8 @@
 const fetch = require('node-fetch');
-const oauth = require('./oauth.js');
 
 exports.handler = async (event, context) => {
-    const {
-        MEETUP_KEY: key,
-        MEETUP_SECRET: secret,
-    } = process.env,
-        redirectUri = 'https://olyjs.com';
-    const authCode = await oauth.getAuthorizationCode(key, redirectUri);
-    const accessToken = await oauth.getAccessToken(key, secret, redirectUri, authCode);
-    console.log(`Authorization code: ${authCode}`);
-    console.log(`Access token: ${accessToken}`);
-
-    const init = {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-    };
-
     try {
-        const res = await fetch('https://api.meetup.com/Olympia-Front-end-Development-Meetup-Group/events', init);
+        const res = await fetch('https://api.meetup.com/Olympia-Front-end-Development-Meetup-Group/events');
         return {
             statusCode: 200,
             headers: {
--- src/components/UpcomingEvents.vue
+++ src/components/UpcomingEvents.vue
@@ -38,7 +38,7 @@ export default {
     };
   },
   mounted() {
-    const baseURI = "https://api.meetup.com/Olympia-Front-end-Development-Meetup-Group/events";
+    const baseURI = "/.netlify/functions/meetup-events";
     this.$http.get(baseURI).then(result => {
       this.events = result.data;
     });
