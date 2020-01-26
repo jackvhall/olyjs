58b77e5716861423aa46ea1f806335c2e79ff1f8
Vincent Hall <jack@olyjs.com>
2020-01-26 10:50:30 -0800

    added mailto link for application

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,9 +4,13 @@
     <p
       class="text-gray-700 mt-2 mb-6"
     >We're always looking for talented individuals to share their knowledge.</p>
-    <button
-      class="bg-black text-white border-2 border-black hover:bg-white hover:text-black py-3 px-6 uppercase text-xs tracking-wide"
-    >Apply</button>
+    <a
+      href="mailto:jack@olyjs.com?subject=Application%20to%20Speak%20at%20OlyJS&body=Tell%20us%20about%20your%20talk"
+    >
+      <button
+        class="bg-black text-white border-2 border-black hover:bg-white hover:text-black py-3 px-6 uppercase text-xs tracking-wide"
+      >Apply</button>
+    </a>
   </section>
 </template>
 
